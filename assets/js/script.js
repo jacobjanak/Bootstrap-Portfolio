7bdec786b9e7580e0bdf53baef6b5c1ed3e64ba7
Jacob Janak <jakeathotmail@gmail.com>
2018-02-12 14:03:44 -0800

    commented my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 
 // Adding spacing when navbar is expanded
 // stupid Bootstrap 4 doesn't do this on its own
-
 var navbarIsOpen = false;
 $('.navbar-toggler').on('click', function() {
   if (!navbarIsOpen) {
@@ -18,9 +17,11 @@ $('.navbar-toggler').on('click', function() {
 
 // Bootstraps collapsible navbar is terrible
 // so I must manually un-collapse it
-
 $(window).on('resize', function() {
   var width = $(window).width();
+
+  // basically making a breakpoint so that the navbar doesn't get messed up
+  // if you resize browser while navbar is un-collapsed
   if (width >= 992) {
     $('.show').removeClass('show')
 
